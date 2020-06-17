b0deea84a2b8eb4fa0c537fb4bd3fc1f0c146f36
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-06-17 13:36:18 +0430

    Initial Commit

---
+++ Speedometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speedometer
+//
+//  Created by Ali Pishvaee on 6/17/20.
+//  Copyright © 2020 AliPishvaee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
