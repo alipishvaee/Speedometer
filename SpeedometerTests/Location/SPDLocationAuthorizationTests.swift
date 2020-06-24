4a18fe7c039cde7eb35cab1a064c2d466d759013
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-06-24 23:24:58 +0430

    add SPDLocationAuthorizationTests to location test group

---
+++ SpeedometerTests/Location/SPDLocationAuthorizationTests.swift
@@ -0,0 +1,17 @@
+//
+//  SPDLocationAuthorizationTests.swift
+//  SpeedometerTests
+//
+//  Created by Ali Pishvaee on 6/24/20.
+//  Copyright © 2020 AliPishvaee. All rights reserved.
+//
+
+import XCTest
+@testable import Speedometer
+class SPDLocationAuthorizationTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+}
