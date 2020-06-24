d9cfd365498aad680916ab641d8211dbe613ad9b
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-06-24 23:47:18 +0430

    basic function setup

--- SpeedometerTests/Location/SPDLocationAuthorizationTests.swift
+++ SpeedometerTests/Location/SPDLocationAuthorizationTests.swift
@@ -10,8 +10,41 @@ import XCTest
 @testable import Speedometer
 class SPDLocationAuthorizationTests: XCTestCase {
 
+    var sut: SPDLocationAuthorization!
+    var locationManagerMock: SPDLocationManagerMock!
+    var delegateMock: SPDLocationAuthorizationDelegateMock!
+    
     override func setUp() {
         super.setUp()
+        locationManagerMock = SPDLocationManagerMock()
+        delegateMock = SPDLocationAuthorizationDelegateMock()
+        
+        sut = SPDDefaultLocationAuthorization(locationManager: locationManagerMock)
+        sut.delegate = delegateMock
     }
 
+    func test_checkAuthorization_notDetermined_requestsAuthorization() {
+        
+    }
+    
+    func test_checkAuthorization_determined_doesNotRequestsAuthorization() {
+        
+    }
+    
+    func test_didChangeAuthorizationStatus_authorizedWhenInUse_notificationIsPosted() {
+        
+    }
+    
+    func test_didChangeAuthorizationStatus_authorizedAlways_notificationIsPosted() {
+        
+    }
+    
+    func test_didChangeAuthorizationStatus_denied_delegateInformed() {
+        
+    }
+    
+    func test_didChangeAuthorizationStatus_restricted_delegateInformed() {
+        
+    }
+    
 }
